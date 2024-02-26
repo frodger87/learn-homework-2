303f067a8db8f60db09baa84c7d334e388874b55
Dmitriy Kostarev <dmitriykostarev9987@gmail.com>
2024-02-26 18:50:14 +0300

    1 add solution; 2add solution; 3 add solution

--- 2_files.py
+++ 2_files.py
@@ -11,12 +11,47 @@
 5. Сохраните результат в файл referat2.txt
 """
 
+PREPOSITIONS = [
+    'в', 'без', 'до', 'для', 'за', 'через', 'над', 'по', 'из',
+    'у', 'около', 'под', 'о', 'про', 'на', 'к', 'перед', 'при',
+    'с', 'между', 'и', 'или', 'а'
+]
+
+
+def open_file(file_path: str):
+    with open(file_path, 'r', encoding='UTF8') as file:
+        return file.read()
+
+
+def remove_prepositions(file):
+    splited_file = file.split()
+    words = []
+    prepositions = PREPOSITIONS
+    for word in splited_file:
+        if word not in prepositions:
+            words.append(word)
+    return words
+
+
+def edit_file(file):
+    return file.replace('.', '!')
+
+
+def save_file(file_text, save_path: str):
+    with open(save_path, 'w', encoding='UTF-8') as f:
+        f.write(file_text)
+
+
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    file_path = '/home/dkostarev/Загрузки/referat.txt'
+    save_path = '/home/dkostarev/Загрузки/referat2.txt'
+    file = open_file(file_path)
+    print(f"Длинна файла: {len(file)}")
+    file_words = remove_prepositions(file)
+    print(f"Количество слов в файле: {len(file_words)}")
+    edited_file = edit_file(file)
+    save_file(edited_file, save_path)
+
 
 if __name__ == "__main__":
     main()
